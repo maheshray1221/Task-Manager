a1fc03d391cb89ec32757053bdf49d3a64f8b5e0
maheshray1221 <mr5832388@gmail.com>
2026-01-24 15:44:26 +0530

    task status complete

--- Frontend/src/components/TaskOverview.jsx
+++ Frontend/src/components/TaskOverview.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { useTaskStore } from "../Store/TaskStore";
 
 export default function TaskOverview() {
-  
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   useEffect(() => {
@@ -19,15 +18,16 @@ export default function TaskOverview() {
   }, [open]);
 
   const [themeState, setThemeState] = useState(0);
-  const [priority, setPriority] = useState("All");
 
   const tasks = useTaskStore((state) => state.tasks);
+  console.log(tasks.length);
 
-  const filterTask = priority=== "All" ? tasks : tasks.filter((task)=>task.priority === priority)
+  const allTaskCount = tasks.length
+  const lowTaskCount = tasks.filter((task) => task.priority === "Low").length;
+  const mediumTaskCount = tasks.filter((task) => task.priority === "Medium",).length;
+  const highTaskCount = tasks.filter((task) => task.priority === "High").length;
   return (
-    
     <div>
-      
       <div className=" w-full overflow-x-hidden min-h-200 md:w-150 md:min-h-screen lg:w-200 lg:min-h-screen lg:ml-5">
         <div className="">
           <div className="flex justify-between md:justify-between lg:justify-between">
@@ -124,7 +124,7 @@ export default function TaskOverview() {
               </div>
             </div>
           )}
-            {filterTask.map((task)=> 
+
           <div className="flex flex-col gap-3 items-center mt-6  md:gap-0 lg:gap-0 md:flex md:flex-row lg:flex lg:flex-row lg:justify-around lg:mt-5 ">
             <div
               className="bg-white w-83 h-25 rounded-2xl border shadow-sm md:w-35 md:h-15 md:rounded-xl md:border-2 md:shadow-sm 
@@ -148,7 +148,7 @@ export default function TaskOverview() {
               </div>
               <div className="ml-5 mt-4 md:ml-2 md:mt-2 lg:ml-3 lg:mt-3">
                 <h1 className="text-lg font-semibold lg:font-normal md:text-sm md:font-normal lg:text-lg">
-                  10
+                  {allTaskCount}
                 </h1>
                 <p className="text-lg md:text-[0.85rem] lg:text-[0.90rem]">
                   All Tasks
@@ -183,7 +183,7 @@ export default function TaskOverview() {
               </div>
               <div className="ml-5 mt-4 md:ml-2 md:mt-2 lg:ml-3 lg:mt-3">
                 <h1 className="text-lg font-semibold lg:font-normal md:text-sm md:font-normal lg:text-lg">
-                  {task.priority}
+                  {lowTaskCount}
                 </h1>
                 <p className="text-lg md:text-[0.85rem] lg:text-[0.90rem]">
                   Low Priority
@@ -217,7 +217,7 @@ export default function TaskOverview() {
               </div>
               <div className="ml-5 mt-4 md:ml-2 md:mt-2 lg:ml-3 lg:mt-3">
                 <h1 className="text-lg font-semibold lg:font-normal md:text-sm md:font-normal lg:text-lg">
-                  {}
+                  {mediumTaskCount}
                 </h1>
                 <p className="text-lg md:text-[0.85rem] lg:text-[0.90rem]">
                   Medium Priority
@@ -251,7 +251,7 @@ export default function TaskOverview() {
               </div>
               <div className="ml-5 mt-4 md:ml-2 md:mt-2 lg:ml-3 lg:mt-3">
                 <h1 className="text-lg font-semibold lg:font-normal md:text-sm md:font-normal lg:text-lg">
-                  2
+                  {highTaskCount}
                 </h1>
                 <p className="text-lg md:text-[0.85rem] lg:text-[0.90rem]">
                   High Priority
@@ -259,7 +259,6 @@ export default function TaskOverview() {
               </div>
             </div>
           </div>
-          )}
         </div>
         <div>
           <TaskList />
