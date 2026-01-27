44332b0368c8cf5cc08a70994920850754c0c7ab
maheshray1221 <mr5832388@gmail.com>
2026-01-27 16:13:37 +0530

    change origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser'
 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://task-manager-frontend-th9q.onrender.com/",
         credentials: true,
 
     }
