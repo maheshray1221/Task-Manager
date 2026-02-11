93795b096e25b5e3f65c6ae805a37eb78579b3df
maheshray1221 <mr5832388@gmail.com>
2026-02-11 12:49:23 +0530

    change origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,13 +4,37 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 
-app.use(cors(
-    {
-        origin:"https://task-manager-frontend-th9q.onrender.com",
-        credentials: true,
 
-    }
-))
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://task-manager-frontend-th9q.onrender.com"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
+
+// app.use(cors(
+//     {
+//         // "https://task-manager-frontend-th9q.onrender.com"
+//         origin:true,
+//         credentials: true,
+
+//     }
+// ))
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.static("public"))
